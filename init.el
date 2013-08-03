43ef03e18dd4a3c9d748512407da7aad95b7d440
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-08-03 16:14:47 +0100

    Useful defuns

--- init.el
+++ init.el
@@ -91,6 +91,12 @@
 (define-key global-map (kbd "M-&") 'vr/query-replace)
 (define-key global-map (kbd "M-/") 'vr/replace)
 
+;; Functions (load all files in defuns-dir)
+(setq defuns-dir (expand-file-name "defuns" user-emacs-directory))
+(dolist (file (directory-files defuns-dir t "\\w+"))
+  (when (file-regular-p file)
+    (load file)))
+
 ;; Run at full power please
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
