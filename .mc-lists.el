c36c5d5892f961b34df40e8d35371d0b23615331
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-08-07 19:42:54 +0100

    mc

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,6 +3,9 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        haskell-indentation-delete-backward-char
+        haskell-indentation-delete-char
+        kill-region-or-backward-word
         org-beginning-of-line
         org-delete-char
         org-delete-char
