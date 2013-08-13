079735e4e12958c1bac9fbbbcf79041c77265309
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-08-13 21:38:20 +0100

    Subatomic-enhanced theme

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+ '(custom-safe-themes (quote ("5bff694d9bd3791807c205d8adf96817ee1e572654f6ddc5e1e58b0488369f9d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
