87a25ad1991ac8644627dcb77637a970285ca8a8
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-08-17 18:10:22 +0100

    Make Agda faces somewhat readable

--- custom.el
+++ custom.el
@@ -9,4 +9,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(agda2-highlight-datatype-face ((t (:foreground "pale turquoise"))) t)
+ '(agda2-highlight-function-face ((t (:foreground "pale turquoise"))) t)
+ '(agda2-highlight-postulate-face ((t (:foreground "indian red"))) t)
+ '(agda2-highlight-primitive-face ((t (:foreground "deep sky blue"))) t)
+ '(agda2-highlight-primitive-type-face ((t (:foreground "pale turquoise"))) t)
+ '(agda2-highlight-record-face ((t (:foreground "chartreuse"))) t))
