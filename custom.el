6931cfa6c75145926da6df8a0811db17acef8792
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-08-24 00:06:15 +0100

    Theme mangling

--- custom.el
+++ custom.el
@@ -3,15 +3,33 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("5bff694d9bd3791807c205d8adf96817ee1e572654f6ddc5e1e58b0488369f9d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+ '(ansi-color-names-vector ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(compilation-message-face (quote default))
+ '(custom-enabled-themes (quote (subatomic-enhanced)))
+ '(custom-safe-themes (quote ("5bff694d9bd3791807c205d8adf96817ee1e572654f6ddc5e1e58b0488369f9d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(default-input-method "japanese-zenkaku")
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
+ '(syslog-debug-face (quote ((t :background unspecified :foreground "#2aa198" :weight bold))))
+ '(syslog-error-face (quote ((t :background unspecified :foreground "#dc322f" :weight bold))))
+ '(syslog-hour-face (quote ((t :background unspecified :foreground "#859900"))))
+ '(syslog-info-face (quote ((t :background unspecified :foreground "#268bd2" :weight bold))))
+ '(syslog-ip-face (quote ((t :background unspecified :foreground "#b58900"))))
+ '(syslog-su-face (quote ((t :background unspecified :foreground "#d33682"))))
+ '(syslog-warn-face (quote ((t :background unspecified :foreground "#cb4b16" :weight bold))))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map (quote ((20 . "#dc322f") (40 . "#CF4F1F") (60 . "#C26C0F") (80 . "#b58900") (100 . "#AB8C00") (120 . "#A18F00") (140 . "#989200") (160 . "#8E9500") (180 . "#859900") (200 . "#729A1E") (220 . "#609C3C") (240 . "#4E9D5B") (260 . "#3C9F79") (280 . "#2aa198") (300 . "#299BA6") (320 . "#2896B5") (340 . "#2790C3") (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list (quote (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(agda2-highlight-datatype-face ((t (:foreground "pale turquoise"))) t)
- '(agda2-highlight-function-face ((t (:foreground "pale turquoise"))) t)
- '(agda2-highlight-postulate-face ((t (:foreground "indian red"))) t)
- '(agda2-highlight-primitive-face ((t (:foreground "deep sky blue"))) t)
- '(agda2-highlight-primitive-type-face ((t (:foreground "pale turquoise"))) t)
- '(agda2-highlight-record-face ((t (:foreground "chartreuse"))) t))
+ '(agda2-highlight-datatype-face ((t (:foreground "pale turquoise"))))
+ '(agda2-highlight-function-face ((t (:foreground "pale turquoise"))))
+ '(agda2-highlight-postulate-face ((t (:foreground "indian red"))))
+ '(agda2-highlight-primitive-face ((t (:foreground "deep sky blue"))))
+ '(agda2-highlight-primitive-type-face ((t (:foreground "pale turquoise"))))
+ '(agda2-highlight-record-face ((t (:foreground "chartreuse")))))
