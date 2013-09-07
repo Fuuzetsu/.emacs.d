12876551f5a0614094d57dc88ce01a7f626d6b4a
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-09-07 20:44:44 +0100

    magit colours

--- setup-magit.el
+++ setup-magit.el
@@ -2,9 +2,9 @@
 (require 'git-rebase-mode)
 
 ;; ;; Subtler highlight
-;; (set-face-background 'magit-item-highlight "#121212")
-;; (set-face-background 'diff-file-header "#121212")
-;; (set-face-foreground 'diff-context "#666666")
+(set-face-background 'magit-item-highlight "#121212")
+(set-face-background 'diff-file-header "#121212")
+(set-face-foreground 'diff-context "#666666")
 ;; (set-face-foreground 'diff-added "#00cc33")
 ;; (set-face-foreground 'diff-removed "#ff0000")
 
