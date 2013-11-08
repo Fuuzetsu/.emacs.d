c7e68b235740496cd4d5229c71dd52d0102565a4
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-11-08 10:45:57 +0000

    Don't bother with track mode.

--- setup-erc.el
+++ setup-erc.el
@@ -27,7 +27,7 @@
 (setq erc-query-display 'buffer)
 
 ;; exclude boring stuff from tracking
-(setq erc-track-mode t)
+(setq erc-track-mode nil)
 (setq erc-track-minor-mode nil) ;; No thanks
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
                                 "324" "329" "332" "333" "353" "477"))
