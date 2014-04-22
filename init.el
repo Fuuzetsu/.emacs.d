53716bf8c37fa0dfefef9656acf0b56ac082ccb4
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-04-22 04:39:09 +0200

    Always white cursor.

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(set-cursor-color "#ffffff")
 
 ;; No splash screen please ... jeez
 (setq inhibit-startup-message t)
